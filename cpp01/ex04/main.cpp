1947faf2deca953d31ef034fad94431d31aa6892
sidev86 <samir.ibrahim@outlook.it>
2024-02-15 10:04:29 +0100

    cpp01 ex04 done

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		*argv = *argv;
+		std::string filename = argv[1];
+		std::string r_filename = filename + ".replace";
+		
+		std::ifstream inputFile(filename.c_str());
+		
+		if (!inputFile)
+		{
+			std::cerr << "Cannot open input file" << std::endl;
+			return 1;
+		}
+		
+		std::ofstream outputFile(r_filename.c_str());
+		
+		if (!outputFile)
+		{
+			std::cerr << "Cannot create output file" << std::endl;
+			return 1;
+		}
+		
+		std::string line;
+		std::string s1 = argv[2];
+		std::string s2 = argv[3];
+		
+		while (std::getline(inputFile, line))
+		{
+			size_t pos = 0;
+			
+			while ((pos = line.find(s1, pos)) != std::string::npos)
+			{
+				line.erase(pos, s1.length());
+				line.insert(pos, s2);
+				pos += s2.length();
+			
+			}
+			outputFile << line << std::endl;
+		}
+		
+		inputFile.close();
+		outputFile.close();
+	}
+	else
+		std::cout << "Error: wrong number of arguments.\nArguments to pass must be: filename, string1(occurrence), string2(substitution)" << std::endl;
+	
+
+	return (0);
+
+}
