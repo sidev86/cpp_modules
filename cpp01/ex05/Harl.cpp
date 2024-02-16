646c7487e2678b3f8fdf0ec44ece9c0e5fcf3531
sidev86 <samir.ibrahim@outlook.it>
2024-02-16 15:29:25 +0100

    added messages to cpp05 ex01

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -13,22 +13,22 @@ Harl::~Harl(void)
 
 void	Harl::debug(void)
 {
-	std::cout << "debug" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "info" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "warning" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "error" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
 
 void	Harl::complain(std::string level)
@@ -43,6 +43,6 @@ void	Harl::complain(std::string level)
 	if (i < 4)
 		(this->*message[i])();
 	else
-		std::cout << "Harl can't complain. Wrong message level" << std::endl;
+		std::cout << "Harl can't complain. Wrong message level\n" << std::endl;
 	
 }
